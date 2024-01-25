138e93a59c1f9af21e434d7778f7ad02e505fbb2
RushabhRamani123 <129403338+RushabhRamani123@users.noreply.github.com>
2024-01-25 23:42:30 +0530

    code- auth api

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 process.on("uncaughtException", (err) => {
   console.log(err);
   console.log("UNCAUGHT Exception! Shutting down ...");
   process.exit(1); // Exit Code 1 indicates that a container shut down, either because of an application failure.
 });
 const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT REJECTION! Shutting down ...");
+})
+
 const http = require("http");
 const server = http.createServer(app);
 
@@ -14,21 +20,24 @@ const server = http.createServer(app);
 //     "<PASSWORD>",
 //     process.env.DATABASE_PASSWORD
 //   );
-//mongodb+srv://rushabhram16:CM0tYIctDRv1DrYd@cluster1.usgm9re.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://rushabhram16:chat_server_project@cluster1.usgm9re.mongodb.net/?retryWrites=true&w=majority
 mongoose.connect
   (
-    `mongodb+srv://rushabhram16:chat_server_project@cluster1.usgm9re.mongodb.net/?retryWrites=true&w=majority`, {}).then(() => { console.log("Database connected"); }).catch((err) => { console.log(err); });
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster1.usgm9re.mongodb.net/?retryWrites=true&w=majority`
+  ,{})
+  .then(() => { console.log("Database connected"); })
+  .catch((err) => { console.log(err); });
 
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`App running on port ${port} ...`);
 });
-
-// process.on("unhandledRejection", (err) => {
-//   console.log(err);
-//   console.log("UNHANDLED REJECTION! Shutting down ...");
-//   server.close(() => {
-//     process.exit(1); //  Exit Code 1 indicates that a container shut down, either because of an application failure.
-//   });
-// });
+// this is the kind of the event listner in express 
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  console.log("UNHANDLED REJECTION! Shutting down ...");
+  server.close(() => {
+    process.exit(1); //  Exit Code 1 indicates that a container shut down, either because of an application failure.
+  });
+});
